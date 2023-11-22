5fdc541c4375027a913da31c8ce0fc151960da7d
Kleber Garcia <kleber.garcia@unity3d.com>
2023-11-22 16:54:41 -0500

    fixing command line

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ set build_type="%~1"
 if %build_type% == "install-dev" (
     py -m pip install --no-build-isolation --editable .
 ) else if %build_type% == "install" (
-    py -m pip install
+    py -m pip install .
 ) else echo build.bat <build-type>: build-type must be dev or install
 
 
